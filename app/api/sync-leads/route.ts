b93594d94c80cad396eb5e10b77485281a8f03b6
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2025-02-27 11:02:56 -0300

    Optmized Prisma to Vercel and add validation of transation

--- app/api/sync-leads/route.ts
+++ app/api/sync-leads/route.ts
@@ -1,38 +1,35 @@
 import { NextResponse } from "next/server";
 import { syncCurrent, syncLeadsRange } from "@/services/leadSyncService";
+import { format, subDays } from "date-fns";
+import { WEBHOOK_URL } from "../leads/route";
+import { getLeadsByLocalDate } from "@/lib/bitrix";
+import { saveLeadsToDatabase } from "@/services/leadService";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const fullSync = searchParams.get("full") === "true";
-
   try {
-    if (fullSync) {
-      try {
-        const rangeLeads = await syncLeadsRange(15);
+    const { searchParams } = new URL(request.url);
+    const fullSync = searchParams.get("full") === "true";
+    const daysToFetch = fullSync ? 5 : 1;
 
-        return NextResponse.json({
-          message: "Full Sync completed ",
-          data: rangeLeads,
-          success: true,
-        });
-      } catch (syncError) {
-        console.error("Error in syncLeadsRange:", syncError);
+    const today = new Date();
+    const leadsByDate: { [date: string]: number } = {};
 
-        return NextResponse.json({
-          message: "Full Sync partially completed with errors",
-          error: (syncError as Error).message,
-          data: [],
-          success: false,
-        });
-      }
-    } else {
-      const result = await syncCurrent();
-      return NextResponse.json({
-        message: "Current day sync completed",
-        data: result,
-        success: true,
-      });
+    for (let i = 0; i < daysToFetch; i++) {
+      const date = format(subDays(today, i), "yyyy-MM-dd");
+      console.log(`Searching leads to ${date}...`);
+      const leads = await getLeadsByLocalDate(WEBHOOK_URL, date);
+      console.log(`Leads found for ${date}: ${leads.length}`);
+
+      const savedCount = await saveLeadsToDatabase(leads);
+      console.log(`Leads saved to ${date}: ${savedCount}`);
+
+      leadsByDate[date] = savedCount;
     }
+
+    return NextResponse.json({
+      message: fullSync ? "Full sync completed" : "Current day sync completed",
+      leadsByDate,
+    });
   } catch (error) {
     console.error("Error in the sync:", error);
     return NextResponse.json(
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,6 +8,10 @@ const prisma =
   global.prisma ||
   new PrismaClient({
     log: ["query", "error", "warn"],
+    transactionOptions: {
+      maxWait: 5000,
+      timeout: 10000,
+    },
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
